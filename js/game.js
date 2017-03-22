e6e7faf4eae676a8d2df457a25c790a16a9162b5
walterje <walterje@edu.aau.at>
2017-03-22 23:04:25 +0100

    Implemented ball out of bounds and automatic ball reset when out of bounds occurs. Starting direction now depends on last out of bounds.

--- js/game.js
+++ js/game.js
@@ -20,7 +20,9 @@ var gameProperties = {
     ballVelocity: 500, // in pixels per second
     ballStartDelay: 2, // in seconds
     ballRandomStartingAngleLeft: [-120, 120],
-    ballRandomStartingAngleRight: [-60, 60]
+    ballRandomStartingAngleRight: [-60, 60],
+
+    scoreToWin: 11
 };
 
 var graphicAssets = {
@@ -56,6 +58,7 @@ var mainState = function (game) {
     this.paddleLeft_down;
     this.paddleRight_up;
     this.paddleRight_down;
+
 };
 
 // The main state that contains our game. Think of states like pages or screens such as the splash screen, main menu, game screen, high scores, inventory, etc.
@@ -127,6 +130,7 @@ mainState.prototype = {
         this.ballSprite.body.collideWorldBounds = true;
         this.ballSprite.body.immovable = true;
         this.ballSprite.body.bounce.set(1); // velocity multiplier after the object collides with something else
+        this.ballSprite.events.onOutOfBounds.add(this.ballOutOfBounds, this); // trigger event when out of bounds
 
         // group both paddles together and set the physics properties for the whole group
         this.paddleGroup = game.add.group();
@@ -147,6 +151,7 @@ mainState.prototype = {
     startDemo: function () {
         this.resetBall();
         this.enablePaddles(false);
+        this.enableBoundaries(true);
         game.input.onDown.add(this.startGame, this);
     },
 
@@ -158,6 +163,13 @@ mainState.prototype = {
 
         // concat both arrays and pick a random value from them
         var randomAngle = game.rnd.pick(gameProperties.ballRandomStartingAngleRight.concat(gameProperties.ballRandomStartingAngleLeft));
+
+        // start ball to the side of the losing player
+        if (this.missedSide === 'right') {
+            randomAngle = game.rnd.pick(gameProperties.ballRandomStartingAngleRight);
+        } else if (this.missedSide === 'left') {
+            randomAngle = game.rnd.pick(gameProperties.ballRandomStartingAngleLeft);
+        }
         game.physics.arcade.velocityFromAngle(randomAngle, gameProperties.ballVelocity, this.ballSprite.body.velocity);
     },
 
@@ -167,6 +179,7 @@ mainState.prototype = {
     startGame: function () {
         game.input.onDown.remove(this.startGame, this);
         this.enablePaddles(true);
+        this.enableBoundaries(false); // disable here, so we can register when the ball goes out of bounds and a player scores
         this.resetBall();
     },
 
@@ -238,23 +251,48 @@ mainState.prototype = {
         // get the segment the ball hit on the paddle. ranges from -3 to 3
         var segmentHit = Math.floor((ball.y - paddle.y)/gameProperties.paddleSegmentHeight);
 
-
+        // check if maximum segment value of 4 is exceeded. do the same for the bottom segments (-4)
+        // subtract 1 from the result for segmentHit, because they range from 0-3
         if (segmentHit >= gameProperties.paddleSegmentsMax) {
             segmentHit = gameProperties.paddleSegmentsMax - 1;
         } else if (segmentHit <= -gameProperties.paddleSegmentsMax - 1) {
             segmentHit = -(gameProperties.paddleSegmentsMax - 1);
         }
 
+        // check whether left or right paddle was hit.
+        // left if horizontal position of the paddle is less than half of the screen, otherwise right paddle
         if (paddle.x < gameProperties.screenWidth * 0.5) {
+            // calculate actual angle by multiplying hit segment by the segment angle constant.
             returnAngle = segmentHit * gameProperties.paddleSegmentAngle;
             game.physics.arcade.velocityFromAngle(returnAngle, gameProperties.ballVelocity, this.ballSprite.body.velocity);
         } else {
+            // offset by 180 degrees if right paddle is hit
             returnAngle = 180 - (segmentHit * gameProperties.paddleSegmentAngle);
             if (returnAngle > 180) {
                 returnAngle -= 360;
             }
             game.physics.arcade.velocityFromAngle(returnAngle, gameProperties.ballVelocity, this.ballSprite.body.velocity);
         }
+    },
+
+    /**
+     * Change the ball's collision behind the paddles
+     * @param enabled: true to enable, false to disable (lets the ball pass through the boundary)
+     */
+    enableBoundaries: function (enabled) {
+        game.physics.arcade.checkCollision.left = enabled;
+        game.physics.arcade.checkCollision.right = enabled;
+    },
+
+    ballOutOfBounds: function () {
+        // check on which side the out of bounds occurred
+        if (this.ballSprite.x < 0) {
+            this.missedSide = 'left';
+        } else if (this.ballSprite.x > gameProperties.screenWidth) { // do we need the if here?
+            this.missedSide = 'right';
+        }
+
+        this.resetBall();
     }
 };
 
